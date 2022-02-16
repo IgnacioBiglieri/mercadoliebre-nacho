c4b5a384a088a4463f3be4ef0c4536b5c2b3bd0a
IgnacioBiglieri <nacho.biglieri00@gmail.com>
2022-02-16 00:46:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando en puerto 3000');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');});
